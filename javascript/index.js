cea83f941c7f49c2b666f49cd78492b6a908919e
Arpit Khandelwal <masterarpit@gmail.com>
2023-01-15 16:12:54 -0500

    update time code

--- javascript/index.js
+++ javascript/index.js
@@ -2,17 +2,11 @@
 function displaydate() {
     var dt = new Date();
     document.getElementById("datevalue").innerHTML = dt.toDateString();
-    document.getElementById("timevalue").innerHTML =
-        dt.getHours() + ":" +
-        dt.getMinutes() + ":" +
-        dt.getSeconds();
+    document.getElementById("timevalue").innerHTML = dt.toLocaleTimeString();
     setInterval(displaydatetime, 1000);
 }
 
 function displaydatetime() {
     var dt = new Date();
-    document.getElementById("timevalue").innerHTML = 
-        dt.getHours() + ":" +
-        dt.getMinutes() + ":" +
-        dt.getSeconds();
+    document.getElementById("timevalue").innerHTML = dt.toLocaleTimeString();
 }
