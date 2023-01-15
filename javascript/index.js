c2cb19bf95fb04d9079b30104b89a14e42765b70
Arpit Khandelwal <masterarpit@gmail.com>
2023-01-14 21:57:19 -0500

    added time on static page

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,18 @@
+
 function displaydate() {
     var dt = new Date();
     document.getElementById("datevalue").innerHTML = dt.toDateString();
+    document.getElementById("timevalue").innerHTML =
+        dt.getHours() + ":" +
+        dt.getMinutes() + ":" +
+        dt.getSeconds();
+    setInterval(displaydatetime, 1000);
+}
+
+function displaydatetime() {
+    var dt = new Date();
+    document.getElementById("timevalue").innerHTML = 
+        dt.getHours() + ":" +
+        dt.getMinutes() + ":" +
+        dt.getSeconds();
 }
